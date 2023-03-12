3cdc4c33e2c0aad18be2e5d0e06b820c3b3494e8
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-03-12 19:22:08 +0530

    new

--- classifier.py
+++ classifier.py
@@ -2,17 +2,35 @@ import xgboost as xgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+
+def predict_model(datatype,seasonal,stationarity,trend):
+
+    model=clf.predict(datatype,seasonal,stationarity,trend)
+
+    return model
+
 
 data=pd.read_csv('Data.csv')
-print(data.head())
+
+le=LabelEncoder()
+
+data["Date_type"]=le.fit_transform(data["Date_type"])
+data["Trend"]=le.fit_transform(data["Trend"])
+data["Seasonal"]=le.fit_transform(data["Seasonal"])
+data["Stationary"]=le.fit_transform(data["Stationary"])
+data["model"]=le.fit_transform(data["model"])
+
 x=data.drop("model",axis=1)
 y=data["model"]
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
 clf = xgb.XGBClassifier()
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
+
 
 acc = accuracy_score(y_test, y_pred)
 print("Accuracy:", acc)
