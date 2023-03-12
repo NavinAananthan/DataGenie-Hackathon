edbaa2f81b23b776a0fbbd6169dbf70a167adc3d
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-03-12 12:00:36 +0530

    Update Model-Classification.py

--- Model-Classification.py
+++ Model-Classification.py
@@ -16,6 +16,87 @@ def check_null(data):
 
     return new_data
         
+    
+    
+def findTrend(df):
+
+    autocorr = acf(df['Value'], nlags=len(df)-1)
+    lag = np.argmax(np.abs(autocorr[1:]) >= 1.96) + 1
+    if lag > 0:
+        bool1=True
+        #print("Trend detected with a period of", lag, "time steps")
+    else:
+        bool1=False
+        #print("No trend detected")
+
+    decomposition = seasonal_decompose(df['Value'], model='additive', period=12)
+    trend = decomposition.trend
+    if trend is not None:
+        bool2=True
+        # print("Trend detected")
+    else:
+        bool2=False
+        # print("No trend detected")
+    
+    return bool1 and bool2
+
+def adfullerTest(value):
+    result_adf = adfuller(value)
+    p_value_adf = result_adf[1]
+    return p_value_adf
+
+def kpssTest(value):
+    result_kpss = kpss(value)
+    p_value_kpss = result_kpss[1]
+    return p_value_kpss
+
+def isSeasonal(df):
+    # perform ADF test
+    adf_val=adfullerTest(df['Value'])
+
+    # perform KPSS test
+    kpss_val=kpssTest(df['Value'])
+
+    # check if p-values are below significance level of 0.05
+    if adf_val < 0.05 and kpss_val > 0.05:
+        seasonal=True
+        # print('The time series data is seasonal.')
+    else:
+        seasonal=False
+        # print('The time series data is not seasonal.')
+
+    return seasonal
+
+
+def isStationary(df):
+
+    adf_val=adfullerTest(df['Value'])
+
+    # perform KPSS test
+    kpss_val=kpssTest(df['Value'])
+
+    # check if p-values are below significance level of 0.05
+    if adf_val < 0.05 and kpss_val < 0.05:
+        stationary=True
+        # print('The time series data is seasonal.')
+    else:
+        stationary=False
+        # print('The time series data is not seasonal.')
+
+    return stationary
+
+def findACF(df):
+    # Compute the autocorrelation coefficients for different lag values
+    autocorr = []
+    for lag in range(1, len(df)):
+        corr = df['Value'].autocorr(lag=lag)
+        autocorr.append(corr)
+
+    # Print the autocorrelation coefficients for each lag
+    for lag, corr in enumerate(autocorr):
+        print(f"Lag {lag+1}: {corr}")
+        
+        
 
 def check_stationarity(data):
 
